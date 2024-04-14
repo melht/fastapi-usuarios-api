e340a63f5c465d8d8dbea6823c8bdf76a3d434ff
patylu001 <patricia.ortegon@gmail.com>
2024-04-14 08:35:51 -0600

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, HTTPException
+from db.db import client
+from controllers.usuariosCRUD import router as usuarios_router
+
+
+app = FastAPI()
+app.include_router(usuarios_router, tags=["usuarios"], prefix="/usuarios")
+# MongoDB connection URL
+@app.on_event("shutdown")
+def shutdown_db_client():
+    client.close()
+
+
+
