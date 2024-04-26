771dc119e6e5994559f1cd3cf8f35cab09e04738
Melanie <meltortolero@gmail.com>
2024-04-25 18:49:58 -0600

    1 Agregando API endpoints

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI, HTTPException
 from db.db import client
-from controllers.usuariosCRUD import router as usuarios_router
+from controller.usuarioCRUD import router as usuarios_router
 
 
 app = FastAPI()
+
 app.include_router(usuarios_router, tags=["usuarios"], prefix="/usuarios")
 # MongoDB connection URL
 @app.on_event("shutdown")
 def shutdown_db_client():
     client.close()
-
-
-
---
+++ model/usuario.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+
+class Usuario(BaseModel):
+    nombre: str
+    email: str
+    password: str
