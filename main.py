3064c216d4b438b53cd558d42f783c1318ef0bdb
melht <105235141+melht@users.noreply.github.com>
2024-05-07 20:00:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, HTTPException
 from db.db import client
 from controller.usuarioCRUD import router as usuarios_router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(usuarios_router, tags=["usuarios"], prefix="/usuarios")
 # MongoDB connection URL
